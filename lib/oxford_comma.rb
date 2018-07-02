b826c8b90e4cfe670897e42a143645c6f1213ac0
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 22:16:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
   sentence = ""
   
-  if
+  if array.length == 1
+    sentence = array[0]
+  elsif array.length == 2
+    sentence = "#{array[0]} and #{array[1]}"
   
   for i in array
     if i == array[-1]
